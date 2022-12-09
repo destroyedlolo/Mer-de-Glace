e6c58bfc99e983fecd9494a6c4ec1c2478ea5e59
destroyedlolo <l_faillie@yahoo.com>
2022-12-09 19:35:28 +0100

    Directory scan on wip

--- src/Directory.cpp
+++ src/Directory.cpp
@@ -4,5 +4,18 @@
 
 #include "Directory.h"
 
+#include <string>
+#include <iostream>
+#include <filesystem>
+
 void Directory::rescan(void){
+	for(const auto & entry : std::filesystem::directory_iterator(this->getName())){
+		if(entry.is_regular_file())
+			putchar('F');
+		else if(entry.is_directory())
+			putchar('D');
+		else	// Ignoring all "special" files
+			continue;
+		printf("-> %s\n", entry.path().c_str());
+	}
 }
